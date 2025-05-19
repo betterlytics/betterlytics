5244045c97bbb610071d18545c3313b9ecda4ef8
Christoffer Woldbye <cwo@rff.dk>
2025-05-19 18:30:41 +0200

    Rename LocalizedText to LocaleText

--- better-analytics-dashboard/src/components/Topbar.tsx
+++ better-analytics-dashboard/src/components/Topbar.tsx
@@ -1,12 +1,12 @@
 import SignOutButton from "@/components/SignOutButton";
-import { LocalizedText } from "@/components/locale/LocalizedText";
+import { LocaleText } from "@/components/locale/LocalizedText";
 import { LocaleSelect } from "./locale/LocaleSelect";
 
 export default function Topbar() {
 
   return (
     <header className="flex items-center justify-end h-14 px-6 bg-white border-b border-gray-200">
-      <LocalizedText
+      <LocaleText
         k="test"
         as="h1"
         className="text-2xl font-bold text-red-600"
--- better-analytics-dashboard/src/components/locale/LocaleText.tsx
+++ better-analytics-dashboard/src/components/locale/LocaleText.tsx
@@ -4,18 +4,18 @@ import React, { ElementType, ComponentPropsWithoutRef } from 'react';
 import { LocalizationKeys } from '@/types/i18n';
 import { useTranslation } from 'react-i18next';
 
-type LocalizedTextProps<T extends ElementType = 'span'> = {
+type LocaleTextProps<T extends ElementType = 'span'> = {
   k: LocalizationKeys;
   as?: T;
   options?: Record<string, unknown>;
 } & ComponentPropsWithoutRef<T>;
 
-export function LocalizedText<T extends ElementType = 'span'>({
+export function LocaleText<T extends ElementType = 'span'>({
   k,
   as,
   options,
   ...props
-}: LocalizedTextProps<T>) {
+}: LocaleTextProps<T>) {
   const { t } = useTranslation();
   const TextComponent = as || 'span';
   return <TextComponent {...props}>{t(k, options)}</TextComponent>;
