805b5f674917a13af76771549097ee9d1727b0e1
jepli <jepli@itu.dk>
2025-07-04 17:26:33 +0200

    Updated change frequency for landing page docs

--- dashboard/src/app/sitemap.ts
+++ dashboard/src/app/sitemap.ts
@@ -51,7 +51,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${baseUrl}/docs`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'weekly',
       priority: 0.8,
     },
 
