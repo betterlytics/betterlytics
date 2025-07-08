62d72ae53a1f6f88425f7e36e345b2b0ce652d5f
Christoffer Romild <woldby3@hotmail.com>
2025-07-08 13:42:45 +0200

    Add FlagIcons

---
+++ dashboard/src/components/icons/FlagIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as Flags from 'country-flag-icons/react/3x2';
+import { getCountryName } from '@/utils/countryCodes';
+
+export type FlagIconProps = {
+  countryCode: keyof typeof Flags;
+  countryName?: string; // Defaults to result of getCountryName
+} & Flags.ElementAttributes<Flags.HTMLSVGElement>;
+
+export const FlagIcon = React.memo(({
+  countryCode,
+  countryName = getCountryName(countryCode),
+  ...props
+}: FlagIconProps) => {
+  const FlagComponent = Flags[countryCode];
+
+  return (
+    <span title={countryName} style={{ display: 'inline-block' }}>
+      <FlagComponent {...props} />
+    </span>
+  );
+});
---
+++ dashboard/src/components/language/CountryDisplay.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { FlagIcon } from '@/components/icons/FlagIcon';
+import { FlagIconProps } from '@/components/icons/FlagIcon';
+import { getCountryName } from '@/utils/countryCodes';
+
+type CountryDisplayProps = {
+  countryCode: FlagIconProps['countryCode'];
+  countryName?: string;  // Defaults to result of getCountryName
+};
+
+export const CountryDisplay = ({ 
+  countryCode, 
+  countryName = getCountryName(countryCode),
+}: CountryDisplayProps) => {
+  return (
+    <div className='m-0 flex items-center gap-2 p-0 overflow-hidden'>
+      <FlagIcon
+        countryCode={countryCode} 
+        countryName={countryName} 
+        className='h-4 w-4 flex-shrink-0' 
+      />
+      <span className='truncate max-w-full'>{countryName}</span>
+    </div>
+  )
+}
