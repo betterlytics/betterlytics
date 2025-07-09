3b2f9d26a73c18e26cd78530b7b89b08b22241ec
Christoffer Romild <woldby3@hotmail.com>
2025-07-09 19:29:25 +0200

    remove fixed w & height on flag-icon in country-display

--- dashboard/src/components/language/CountryDisplay.tsx
+++ dashboard/src/components/language/CountryDisplay.tsx
@@ -15,8 +15,8 @@ export const CountryDisplay = ({
     <div className='m-0 flex items-center gap-2 p-0 overflow-hidden'>
       <FlagIcon
         countryCode={countryCode} 
-        countryName={countryName} 
-        className='h-4 w-4 flex-shrink-0' 
+        countryName={countryName}
+        className='flex-shrink-0' 
       />
       <span className='truncate max-w-full'>{countryName}</span>
     </div>
