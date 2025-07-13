e0b1c951b97958252fce04d219963b92540ee60e
Lindharden <jeppe@kripet.dk>
2025-07-13 19:14:03 +0200

    Dashboard Overview docs (#355)

--- docs/src/content/dashboard/_meta.js
+++ docs/src/content/dashboard/_meta.js
@@ -1,9 +1,10 @@
 export default {
   index: "Dashboard Overview",
-  "metrics-glossary": "Metrics Glossary",
   referrers: "Referrers",
   geography: "Geography",
   "user-journey": "User Journey Page",
   devices: "Device Breakdown",
   events: "Events Page",
+  funnels: "Funnels",
+  "metrics-glossary": "Metrics Glossary",
 };
--- docs/src/mdx-components.js
+++ docs/src/mdx-components.js
@@ -1,10 +1,16 @@
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs";
+import {
+  DashboardSectionGrid,
+  DashboardSectionCard,
+} from "./app/components/DashboardSectionGrid";
 
 const themeComponents = getThemeComponents();
 
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
+    DashboardSectionGrid,
+    DashboardSectionCard,
     ...components,
   };
 }
