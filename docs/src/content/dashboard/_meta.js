aed8e554102b8e194847f395f43922a6e5fe4f47
Lindharden <jeppe@kripet.dk>
2025-07-13 14:28:01 +0200

    Metric Glossary docs (#342)

--- docs/src/content/dashboard/_meta.js
+++ docs/src/content/dashboard/_meta.js
@@ -1,5 +1,6 @@
 export default {
   index: "Dashboard Overview",
+  "metrics-glossary": "Metrics Glossary",
   referrers: "Referrers",
   geography: "Geography",
   "user-journey": "User Journey Page",
