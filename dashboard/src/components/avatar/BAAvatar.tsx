c09f60d3fada9137d7991ca8a2aa6cec0d64c3f1
Thomas <thomas.schauser@gmail.com>
2025-07-13 16:59:53 +0200

    Added custom BAAvatar wrapper

---
+++ dashboard/src/components/avatar/BAAvatar.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useUserSettings } from '@/hooks/useUserSettings';
+import { useMemo } from 'react';
+import { Avatar } from '../ui/avatar';
+import { User } from 'lucide-react';
+import { GravatarImage } from '../ui/gravatar-image';
+import { useSession } from 'next-auth/react';
+
+export function BAAvatar() {
+  const { settings } = useUserSettings();
+  const avatarMode = useMemo(() => settings?.avatar || 'default', [settings?.avatar]);
+
+  return (
+    <Avatar className='relative h-8 w-8'>
+      {avatarMode === 'default' ? (
+        <DefaultAvatar />
+      ) : avatarMode === 'gravatar' ? (
+        <GravatarAvatar />
+      ) : (
+        <DefaultAvatar />
+      )}
+    </Avatar>
+  );
+}
+
+function DefaultAvatar() {
+  return <User className='bg-muted text-muted-foreground size-full p-2' />;
+}
+
+function GravatarAvatar() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return <DefaultAvatar />;
+  }
+
+  return (
+    <>
+      <DefaultAvatar />
+      <GravatarImage email={session.user?.email} alt={session.user?.name || 'User'} className='bg-red absolute' />
+    </>
+  );
+}
