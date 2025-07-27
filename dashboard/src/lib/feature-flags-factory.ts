dab0583cd64d66edab013f222069969ad1cdbbe2
Thomas Schauser <thomas.schauser@gmail.com>
2025-07-27 21:05:27 +0200

    Added public action for fetching public environment variables (#397)

---
+++ dashboard/src/lib/feature-flags-factory.ts
@@ -0,0 +1,13 @@
+import type { env } from '@/lib/env';
+
+type FeatureFlagEnvironmentKeys = 'PUBLIC_IS_CLOUD';
+export type FeatureFlagEnvironment = {
+  [K in FeatureFlagEnvironmentKeys]: (typeof env)[K];
+};
+
+export function createFeatureFlags(environment: FeatureFlagEnvironment) {
+  return {
+    enableBilling: environment.PUBLIC_IS_CLOUD,
+    isCloud: environment.PUBLIC_IS_CLOUD,
+  } as const;
+}
