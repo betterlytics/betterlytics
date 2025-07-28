2cc95ada0cc024b1731c99595d4d0d3010e51b14
Thomas <thomas.schauser@gmail.com>
2025-07-28 10:33:57 +0200

    Removed default from account type

--- dashboard/prisma/migrations/20250728083220_updated_account_schema_for_adapter/migration.sql
+++ dashboard/prisma/migrations/20250728083220_updated_account_schema_for_adapter/migration.sql
@@ -9,6 +9,7 @@
   - You are about to drop the `VerificationRequest` table. If the table is not empty, all the data it contains will be lost.
   - A unique constraint covering the columns `[provider,providerAccountId]` on the table `Account` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `provider` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `Account` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -31,7 +32,7 @@ ADD COLUMN     "refresh_token" TEXT,
 ADD COLUMN     "scope" TEXT,
 ADD COLUMN     "session_state" TEXT,
 ADD COLUMN     "token_type" TEXT,
-ADD COLUMN     "type" TEXT NOT NULL DEFAULT 'credentials';
+ADD COLUMN     "type" TEXT NOT NULL;
 
 -- DropTable
 DROP TABLE "VerificationRequest";
