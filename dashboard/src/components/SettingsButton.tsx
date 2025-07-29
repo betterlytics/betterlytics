4d5b2b6d90d37b83ea623a076646fc6e5551eee2
Ramy Aboul Naga <ramy.naga@gmail.com>
2025-07-29 23:22:38 +0200

    revert: remove quick settings popover

--- dashboard/src/components/SettingsButton.tsx
+++ dashboard/src/components/SettingsButton.tsx
@@ -1,24 +1,42 @@
 'use client';
 
+import SettingsPopover from './SettingsPopover';
 import { Settings } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import DashboardSettingsDialog from '@/components/dashboardSettings/DashboardSettingsDialog';
 import { useSearchParamKey } from '@/hooks/use-search-param-key';
 
 export default function SettingsButton() {
-  const [showSettingsDialog, setShowSettingsDialog] = useSearchParamKey('settings');
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useSearchParamKey('settings');
+
+  const handleAdvancedSettingsClicked = () => {
+    setIsDialogOpen(true);
+    setIsPopoverOpen(false);
+  };
 
   return (
     <>
-      <Button
-        variant='ghost'
-        onClick={() => setShowSettingsDialog(true)}
-        className='hover:bg-accent hover:text-accent-foreground text-foreground flex w-full items-center gap-2 rounded px-2 py-2 text-sm font-medium'
-      >
-        <Settings size={18} />
-        Dashboard Settings
-      </Button>
-      <DashboardSettingsDialog open={showSettingsDialog} onOpenChange={setShowSettingsDialog} />
+      <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant='ghost'
+            className='hover:bg-accent hover:text-accent-foreground text-foreground flex w-full items-center gap-2 rounded px-2 py-2 text-sm font-medium'
+          >
+            <Settings size={18} />
+            Dashboard Settings
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className='w-auto border-none p-0 shadow-lg' side='top' align='start'>
+          <SettingsPopover
+            onAdvancedSettingsClicked={handleAdvancedSettingsClicked}
+            onClose={() => setIsPopoverOpen(false)}
+          />
+        </PopoverContent>
+      </Popover>
+      <DashboardSettingsDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
     </>
   );
 }
