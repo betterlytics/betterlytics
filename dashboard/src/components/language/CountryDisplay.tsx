ff3545a2721531bdc539bad945562391a9683ed5
Ramy Aboul Naga <ramy.naga@gmail.com>
2025-07-30 23:35:33 +0200

    refactor: move aspect ratio to FlagIcon

--- dashboard/src/components/language/CountryDisplay.tsx
+++ dashboard/src/components/language/CountryDisplay.tsx
@@ -19,7 +19,7 @@ export const CountryDisplay = ({
       <FlagIcon
         countryCode={countryCode} 
         countryName={countryName}
-        className='flex-shrink-0 aspect-[3/2] w-auto!' 
+        className='flex-shrink-0' 
       />
       <span className='truncate max-w-full'>{countryName}</span>
     </div>
