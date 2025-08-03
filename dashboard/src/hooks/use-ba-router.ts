426975a17780bc8f03f220b1f8fbfa6d655e1549
Christoffer Woldbye <cwo@rff.dk>
2025-08-03 12:05:21 +0200

    Wrap BA-router in useCallbacks

--- dashboard/src/hooks/use-ba-router.ts
+++ dashboard/src/hooks/use-ba-router.ts
@@ -2,20 +2,27 @@
 
 import { useRouter } from 'next/navigation';
 import { useTopLoader } from 'nextjs-toploader';
+import { useCallback } from 'react';
 
 export function useBARouter() {
   const router = useRouter();
   const { start } = useTopLoader();
 
-  const push = async (...args: Parameters<typeof router.push>) => {
-    start();
-    return router.push(...args);
-  };
+  const push = useCallback(
+    async (...args: Parameters<typeof router.push>) => {
+      start();
+      return router.push(...args);
+    },
+    [router, start],
+  );
 
-  const replace = async (...args: Parameters<typeof router.replace>) => {
-    start();
-    return router.replace(...args);
-  };
+  const replace = useCallback(
+    async (...args: Parameters<typeof router.replace>) => {
+      start();
+      return router.replace(...args);
+    },
+    [router, start],
+  );
 
   return {
     ...router,
