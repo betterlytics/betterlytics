74081bac9e4aef0df935fd24ee1ee37f343f931e
Thomas Schauser <thomas.schauser@gmail.com>
2025-08-03 15:31:14 +0200

    Building dashboard Docker image (#411)

--- dashboard/src/lib/env.ts
+++ dashboard/src/lib/env.ts
@@ -1,6 +1,12 @@
 import { SUPPORTED_LANGUAGES } from '@/constants/supportedLanguages';
 import { z } from 'zod';
 
+export const zStringBoolean = z
+  .enum(['true', 'false'])
+  .optional()
+  .default('false')
+  .transform((val) => val === 'true');
+
 const envSchema = z.object({
   CLICKHOUSE_URL: z.string().url(),
   CLICKHOUSE_DASHBOARD_USER: z.string().min(1),
@@ -11,60 +17,22 @@ const envSchema = z.object({
   PUBLIC_ANALYTICS_BASE_URL: z.string().min(1),
   NEXTAUTH_URL: z.string().url().optional(),
   NEXTAUTH_SECRET: z.string().min(1),
-  ENABLE_DASHBOARD_TRACKING: z
-    .enum(['true', 'false'])
-    .optional()
-    .default('false')
-    .transform((val) => val === 'true'),
-  ENABLE_REGISTRATION: z
-    .enum(['true', 'false'])
-    .optional()
-    .default('false')
-    .transform((val) => val === 'true'),
-  PUBLIC_BASE_URL: z.string().optional().default('http://localhost:3000'),
-  PUBLIC_IS_CLOUD: z
-    .enum(['true', 'false'])
-    .default('false')
-    .transform((val) => val === 'true'),
-  IS_CLOUD: z
-    .enum(['true', 'false'])
-    .optional()
-    .default('false')
-    .transform((val) => val === 'true'),
-  ENABLE_BILLING: z
-    .enum(['true', 'false'])
-    .optional()
-    .default('false')
-    .transform((val) => val === 'true'),
+  ENABLE_DASHBOARD_TRACKING: zStringBoolean,
+  ENABLE_REGISTRATION: zStringBoolean,
+  PUBLIC_BASE_URL: z.string().optional().default('https://betterlytics.io'),
+  PUBLIC_IS_CLOUD: zStringBoolean,
+  IS_CLOUD: zStringBoolean,
+  ENABLE_BILLING: zStringBoolean,
   PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().optional().default(''),
   STRIPE_SECRET_KEY: z.string().optional().default(''),
   STRIPE_WEBHOOK_SECRET: z.string().optional().default(''),
-  ENABLE_EMAILS: z
-    .enum(['true', 'false'])
-    .optional()
-    .default('false')
-    .transform((val) => val === 'true'),
+  ENABLE_EMAILS: zStringBoolean,
   MAILER_SEND_API_TOKEN: z.string().optional().default(''),
-  ENABLE_MAIL_PREVIEW_PAGE: z
-    .enum(['true', 'false'])
-    .optional()
-    .default('false')
-    .transform((val) => val === 'true'),
-  ENABLE_ACCOUNT_VERIFICATION: z
-    .enum(['true', 'false'])
-    .optional()
-    .default('false')
-    .transform((val) => val === 'true'),
+  ENABLE_MAIL_PREVIEW_PAGE: zStringBoolean,
+  ENABLE_ACCOUNT_VERIFICATION: zStringBoolean,
   TOTP_SECRET_ENCRYPTION_KEY: z.string().length(32),
-  ENABLE_MONITORING: z
-    .enum(['true', 'false'])
-    .optional()
-    .default('false')
-    .transform((val) => val === 'true'),
-  ENABLE_APP_TRACKING: z
-    .enum(['true', 'false'])
-    .default('false')
-    .transform((val) => val === 'true'),
+  ENABLE_MONITORING: zStringBoolean,
+  ENABLE_APP_TRACKING: zStringBoolean,
   APP_TRACKING_SITE_ID: z.string().optional(),
   DEFAULT_LANGUAGE: z.enum(SUPPORTED_LANGUAGES).optional().default('en'),
   GITHUB_ID: z.string().optional().default(''),
