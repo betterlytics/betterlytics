e81b3871b00acc5911542b5709ba157af05acc46
Christoffer Romild <woldby3@hotmail.com>
2025-08-25 23:10:06 +0200

    Remove default from country display

--- dashboard/src/components/language/CountryDisplay.tsx
+++ dashboard/src/components/language/CountryDisplay.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { FlagIcon, FlagIconProps } from '@/components/icons';
-import { getCountryName } from '@/utils/countryCodes';
 import { cn } from '@/lib/utils';
-import { useLocale } from 'next-intl';
 
 type CountryDisplayProps = {
   countryCode: FlagIconProps['countryCode'];
-  countryName?: string; // Defaults to result of getCountryName w. current locale
+  countryName: string;
   className?: string;
 };
 
 export const CountryDisplay = ({ countryCode, countryName, className }: CountryDisplayProps) => {
-  if (!countryName) {
-    const locale = useLocale();
-    console.log('locale is', locale);
-    countryName = getCountryName(countryCode, locale);
-  }
   return (
     <div className={cn(className, 'm-0 flex items-center gap-2 overflow-hidden p-0')}>
       <FlagIcon countryCode={countryCode} countryName={countryName} className='flex-shrink-0' />
