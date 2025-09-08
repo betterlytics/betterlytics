ee39a90a2c351e6a1b012405d55a47f7c42c0eab
Thomas <thomas.schauser@gmail.com>
2025-09-08 18:51:47 +0200

    Added larger gap

--- dashboard/src/components/dashboard/DashboardHeader.tsx
+++ dashboard/src/components/dashboard/DashboardHeader.tsx
@@ -7,7 +7,7 @@ interface DashboardHeaderProps {
 
 export function DashboardHeader({ title, children }: DashboardHeaderProps) {
   return (
-    <div className='mb-2 flex flex-col justify-between gap-2 lg:flex-row'>
+    <div className='mb-4 flex flex-col justify-between gap-2 lg:flex-row'>
       <h1 className='pt-1 text-2xl font-semibold sm:pl-1'>{title}</h1>
       {children}
     </div>
