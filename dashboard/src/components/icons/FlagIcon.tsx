1c8d7e0cd50b930c870eb8fd421c8c847196aee3
Thomas <thomas.schauser@gmail.com>
2025-09-11 19:48:13 +0200

    Fixed language select country flag

--- dashboard/src/components/icons/FlagIcon.tsx
+++ dashboard/src/components/icons/FlagIcon.tsx
@@ -29,7 +29,7 @@ function FlagIconComponent({ countryCode, countryName, ...props }: FlagIconProps
     <span title={countryName || 'Unknown'} className='flex items-center justify-center'>
       <FlagComponent
         {...props}
-        className='shadow-foreground/80 dark:shadow-background/50 !h-[1.15em] w-fit rounded-xs shadow-sm dark:rounded-none'
+        className='shadow-foreground/80 dark:shadow-background/50 !h-[1.15em] !w-fit rounded-xs shadow-sm dark:rounded-none'
         style={{
           imageRendering: 'auto',
           shapeRendering: 'geometricPrecision',
