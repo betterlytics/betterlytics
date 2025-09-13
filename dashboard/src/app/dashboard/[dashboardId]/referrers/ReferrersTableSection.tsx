b023f4157f41d590145df990ea8b9b30d177c967
Lindharden <jeppe@kripet.dk>
2025-09-13 12:20:22 +0200

    added shadcn card to referrers table section

--- dashboard/src/app/dashboard/[dashboardId]/referrers/ReferrersTableSection.tsx
+++ dashboard/src/app/dashboard/[dashboardId]/referrers/ReferrersTableSection.tsx
@@ -4,6 +4,7 @@ import { use } from 'react';
 import ReferrerTable from '@/app/dashboard/[dashboardId]/referrers/ReferrerTable';
 import { fetchReferrerTableDataForSite } from '@/app/actions';
 import { useTranslations } from 'next-intl';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 type ReferrersTableSectionProps = {
   referrerTablePromise: ReturnType<typeof fetchReferrerTableDataForSite>;
@@ -15,9 +16,13 @@ export default function ReferrersTableSection({ referrerTablePromise }: Referrer
   const t = useTranslations('components.referrers.table');
 
   return (
-    <div className='bg-card border-border rounded-xl border px-3 py-4 shadow sm:px-6'>
-      <div className='text-foreground mb-2 px-0 text-lg font-medium'>{t('details')}</div>
-      <ReferrerTable data={tableData} />
-    </div>
+    <Card className='border-border flex min-h-[300px] flex-col gap-1 p-2 sm:min-h-[400px] sm:px-6 sm:pt-3'>
+      <CardHeader className='px-0 pb-0'>
+        <CardTitle className='text-lg font-medium'>{t('details')}</CardTitle>
+      </CardHeader>
+      <CardContent className='px-0'>
+        <ReferrerTable data={tableData} />
+      </CardContent>
+    </Card>
   );
 }
