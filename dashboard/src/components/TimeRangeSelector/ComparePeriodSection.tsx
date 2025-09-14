106aabf894c8cd3d8533fcd833a5ec1061c25187
Lindharden <jeppe@kripet.dk>
2025-09-14 10:22:07 +0200

    removed redundant css

--- dashboard/src/components/TimeRangeSelector/ComparePeriodSection.tsx
+++ dashboard/src/components/TimeRangeSelector/ComparePeriodSection.tsx
@@ -41,7 +41,7 @@ export function ComparePeriodSection({
         <>
           <Separator className='my-4' />
           <div>
-            <h3 className='text-text mb-2 text-sm font-medium'>{t('compareToPeriod')}</h3>
+            <h3 className='mb-2 text-sm font-medium'>{t('compareToPeriod')}</h3>
             <div className='grid gap-4'>
               <DateRangePicker
                 onDateRangeSelect={(range) => {
