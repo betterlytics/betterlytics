f4fdd000b85480f0a6f6a70e3c6a0bc2a772f396
Thomas <thomas.schauser@gmail.com>
2025-09-20 11:54:07 +0200

    Updated migrations

--- migrations/10_add_scroll_depth_column.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE analytics.events
-    MODIFY COLUMN event_type Enum8('pageview' = 1, 'custom' = 2, 'scroll_depth' = 3),
-    ADD COLUMN IF NOT EXISTS scroll_depth Nullable(Float32) AFTER custom_event_json;
---
+++ migrations/12_add_scroll_depth_column.sql
@@ -0,0 +1,3 @@
+ALTER TABLE analytics.events
+    MODIFY COLUMN event_type Enum8('pageview' = 1, 'custom' = 2, 'outbound_link' = 3, 'cwv' = 4, 'scroll_depth' = 5),
+    ADD COLUMN IF NOT EXISTS scroll_depth Nullable(Float32) AFTER custom_event_json;
