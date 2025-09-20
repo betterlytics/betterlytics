eeee0e86626970d9f4ec8837f1b6ce88a76992b0
Thomas <thomas.schauser@gmail.com>
2025-09-20 13:49:08 +0200

    setting interval as custom on custom range selection

--- dashboard/src/components/TimeRange/hooks/useImmediateTimeRange.ts
+++ dashboard/src/components/TimeRange/hooks/useImmediateTimeRange.ts
@@ -56,6 +56,7 @@ export function useImmediateTimeRange() {
       const alignedStart = getStartDateWithGranularity(startDate, nextGranularity);
       const alignedEnd = getEndDateWithGranularity(endDate, nextGranularity);
       ctx.setPeriod(alignedStart, alignedEnd);
+      ctx.setInterval('custom');
       if (ctx.granularity !== nextGranularity) ctx.setGranularity(nextGranularity);
     },
     [ctx],
