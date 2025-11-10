e06e911da9b934bb51fd91bdcf35f1b5230e31c1
Christoffer Romild <woldby3@hotmail.com>
2025-11-10 09:16:26 +0100

    Rewrite zod enum validations to use native prisma/client enums where appropriate

--- dashboard/src/entities/authContext.ts
+++ dashboard/src/entities/authContext.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
+import { DashboardRole } from '@prisma/client';
 
 export const AuthContextSchema = z.object({
   dashboardId: z.string(),
   userId: z.string(),
   siteId: z.string(),
-  role: z.string()
+  role: z.nativeEnum(DashboardRole)
 });
 export type AuthContext = z.infer<typeof AuthContextSchema>;
