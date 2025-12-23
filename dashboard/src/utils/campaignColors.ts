a27a239a891660c3cc5f682d03549f6bb4d2712f
Evan Yang <evanjyang4@gmail.com>
2025-12-22 21:23:53 -0800

    generalize colors

--- dashboard/src/utils/campaignColors.ts
+++ dashboard/src/utils/campaignColors.ts
@@ -1,13 +1,11 @@
+import { getColorForValue } from './colorUtils';
+
+/**
+ * Get a consistent color for a campaign source name.
+ * Uses RGB format for better compatibility with existing charts.
+ */
 export function getCampaignSourceColor(sourceName: string): string {
-  // For better results, we should probably use a more sophisticated
-  // color generation algorithm (for instance, one based on HSL with varying hue)
-  let hash = 0;
-  for (let i = 0; i < sourceName.length; i++) {
-    hash = sourceName.charCodeAt(i) + ((hash << 5) - hash);
-    hash = hash & hash; // Convert to 32bit integer
-  }
-  const r = (hash & 0xff0000) >> 16;
-  const g = (hash & 0x00ff00) >> 8;
-  const b = hash & 0x0000ff;
-  return `rgb(${r}, ${g}, ${b})`;
+  return getColorForValue(sourceName, {
+    format: 'rgb',
+  });
 }
