0673c022e7ff80338c358b37f69d955592a63a88
Thomas <thomas.schauser@gmail.com>
2026-01-11 11:18:24 +0100

    Fixed styling of PrefixInput

--- dashboard/src/components/inputs/PrefixInput.tsx
+++ dashboard/src/components/inputs/PrefixInput.tsx
@@ -8,8 +8,8 @@ type PrefixInputProps = ComponentProps<typeof Input> & {
 
 export function PrefixInput({ prefix, className, ...restProps }: PrefixInputProps) {
   return (
-    <div className='bg-input flex items-center rounded-md'>
-      <div className='text-muted-foreground border-card-foreground/20 bg-card-foreground/20 flex h-10 items-center rounded-s-md border px-2 text-sm'>
+    <div className='flex items-center rounded-md'>
+      <div className='text-muted-foreground/90 border-card-foreground/20 bg-muted flex h-10 items-center rounded-s-md border px-2 text-sm'>
         {prefix}
       </div>
       <Input {...restProps} className={cn('h-10 rounded-s-none rounded-e-md', className)} />
