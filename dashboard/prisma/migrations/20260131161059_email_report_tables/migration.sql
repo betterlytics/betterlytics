a419a4529ba6df47d0bed73bbc482a59de7daf58
Lindharden <jeppe@kripet.dk>
2026-01-31 17:12:05 +0100

    Squashed migration

--- dashboard/prisma/migrations/20260131161059_email_report_tables/migration.sql
+++ dashboard/prisma/migrations/20260131161059_email_report_tables/migration.sql
@@ -1,7 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `reportRecipients` on the `DashboardSettings` table. All the data in the column will be lost.
+
+*/
 -- AlterTable
-ALTER TABLE "DashboardSettings" ADD COLUMN     "lastMonthlyReportSentAt" TIMESTAMP(3),
+ALTER TABLE "DashboardSettings" DROP COLUMN "reportRecipients",
+ADD COLUMN     "lastMonthlyReportSentAt" TIMESTAMP(3),
 ADD COLUMN     "lastWeeklyReportSentAt" TIMESTAMP(3),
+ADD COLUMN     "monthlyReportRecipients" TEXT[] DEFAULT ARRAY[]::TEXT[],
 ADD COLUMN     "weeklyReportDay" INTEGER NOT NULL DEFAULT 1,
+ADD COLUMN     "weeklyReportRecipients" TEXT[] DEFAULT ARRAY[]::TEXT[],
 ALTER COLUMN "weeklyReports" SET DEFAULT false;
 
 -- CreateTable
