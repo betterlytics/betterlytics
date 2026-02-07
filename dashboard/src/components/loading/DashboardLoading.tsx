883713397bc1ba49d104df9e9b99082532309b99
Christoffer Romild <woldby3@hotmail.com>
2026-02-07 01:58:04 +0100

    - Remove unused locale key - Remove unneccessary wrapper div

--- dashboard/src/components/loading/DashboardLoading.tsx
+++ dashboard/src/components/loading/DashboardLoading.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import React from 'react';
-import BetterlyticsLoading from '@/components/loading/BetterlyticsLoading';
+import BetterlyticsLogoLoading from '@/components/loading/BetterlyticsLogoLoading';
 import { useTranslations } from 'next-intl';
 
 export default function DashboardLoading() {
   const t = useTranslations('components.loading.dashboard');
-  return <BetterlyticsLoading title={t('initializing')} description={t('loadingSettings')} className='min-h-screen' />;
+  return <BetterlyticsLogoLoading title={t('initializing')} description={t('loadingSettings')} className='min-h-screen' />;
 }
