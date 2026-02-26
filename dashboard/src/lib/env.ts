62cb65c7aa892e3deef9049d56f8235ad1ec2afa
jepli <jepli@itu.dk>
2026-02-26 19:33:49 +0100

    Removed integrations toggle flag

--- dashboard/src/lib/env.ts
+++ dashboard/src/lib/env.ts
@@ -63,8 +63,7 @@ const envSchema = z.object({
   BACKGROUND_JOBS_ENABLED: zStringBoolean,
   IS_DEVELOPMENT: zStringBoolean,
   PUSHOVER_APP_TOKEN: z.string().optional(),
-  INTEGRATION_ENCRYPTION_KEY: z.string().length(32),
-  PUBLIC_ENABLE_INTEGRATIONS: zStringBoolean,
+  INTEGRATION_ENCRYPTION_KEY: z.string().length(32)
 });
 
 export const env = envSchema.parse(process.env);
