516308afa4c02440fcdb7a66e3590c741a12bc86
Thomas <thomas.schauser@gmail.com>
2026-02-27 17:34:32 +0100

    Fixed timezone fallback to guess

--- dashboard/src/app/(protected)/TimezoneCookieInitializer.tsx
+++ dashboard/src/app/(protected)/TimezoneCookieInitializer.tsx
@@ -3,13 +3,23 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { setTimezoneCookieAction } from '@/app/actions/system/timezone.action';
+import moment from 'moment-timezone';
+
+function resolveTimezone() {
+  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  if (!tz || tz === 'Etc/Unknown') {
+    return moment.tz.guess();
+  }
+
+  return tz;
+}
 
 export default function TimezoneCookieInitializer() {
   const router = useRouter();
 
   useEffect(() => {
-    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    if (!tz) return;
+    const tz = resolveTimezone();
     setTimezoneCookieAction(tz).then((res) => {
       if (res.changed) {
         router.refresh();
