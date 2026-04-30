be4ecb27ce59a7986be713da19391187b0224b96
Thomas <thomas.schauser@gmail.com>
2026-04-30 20:44:16 +0200

    fixed timezone

--- dashboard/src/lib/cookies.ts
+++ dashboard/src/lib/cookies.ts
@@ -1,9 +1,10 @@
 'use server';
 
 import { cookies } from 'next/headers';
+import { BATimeZone } from '@/entities/analytics/analyticsQuery.entities';
 
 export async function getUserTimezone() {
   const cookieStore = await cookies();
   const timezone = cookieStore.get('bl_tz')?.value;
-  return timezone ?? 'Etc/UTC';
+  return BATimeZone.parse(timezone ?? 'Etc/UTC');
 }
