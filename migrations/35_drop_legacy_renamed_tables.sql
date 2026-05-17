a2726499bd0debf282f9ac3f65678eef8b804265
Lindharden <jeppe@kripet.dk>
2026-05-17 17:27:43 +0200

    Fixed missing global property insertion in session MV (#935)

--- migrations/35_drop_legacy_renamed_tables.sql
+++
@@ -1,5 +0,0 @@
--- Drop legacy tables left behind by previous schema migrations
-DROP TABLE IF EXISTS analytics.events_old;
-DROP TABLE IF EXISTS analytics.events_old_2;
-DROP TABLE IF EXISTS analytics.session_replays_old;
-DROP TABLE IF EXISTS analytics.session_replays_old_2;
