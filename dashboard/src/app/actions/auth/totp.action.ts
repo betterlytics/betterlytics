6eb9bed20592a066418cf1022081fbf5c0ec55e8
Lindharden <jeppe@kripet.dk>
2026-05-24 20:26:04 +0200

    Added TOTP requirement to disable TOTP (#955)

--- dashboard/src/app/actions/auth/totp.action.ts
+++ dashboard/src/app/actions/auth/totp.action.ts
@@ -12,6 +12,6 @@ export const enableTotpAction = withUserAuth(async (user: User, totp: string): P
   return await TotpService.enableTotp(user.id, totp);
 });
 
-export const disableTotpAction = withUserAuth(async (user: User): Promise<void> => {
-  return await TotpService.disableTotp(user.id);
+export const disableTotpAction = withUserAuth(async (user: User, totp: string): Promise<void> => {
+  return await TotpService.disableTotp(user.id, totp);
 });
