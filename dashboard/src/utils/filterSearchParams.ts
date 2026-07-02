b33aca709b267ebb8208a11c601f014dc9dea006
Christoffer Romild <woldby3@hotmail.com>
2026-07-02 16:17:56 +0200

    Default stepFilters when decoding filters from search params

--- dashboard/src/utils/filterSearchParams.ts
+++ dashboard/src/utils/filterSearchParams.ts
@@ -49,7 +49,6 @@ function getDefaultFilters(): FilterQueryParams {
     userJourney: {
       numberOfSteps: 3,
       numberOfJourneys: 5,
-      stepFilters: {},
     },
     compareStartDate: range.compare?.start,
     compareEndDate: range.compare?.end,
@@ -247,6 +246,7 @@ function decode(params: FilterQuerySearchParams, timezone: string): BAAnalyticsQ
 
   return {
     ...validated,
+    userJourney: { ...validated.userJourney, stepFilters: {} },
     timezone,
   };
 }
