d7c2cc4676d40740f9ece15069832d36ba0b7839
Thomas <thomas.schauser@gmail.com>
2026-08-04 20:40:10 +0200

    Made nicer

--- dashboard/src/components/filters/CountryFlagLabel.tsx
+++ dashboard/src/components/filters/CountryFlagLabel.tsx
@@ -15,9 +15,13 @@ export function CountryFlagLabel({ countryCode, className, children }: CountryFl
   const code = countryCode.toUpperCase();
 
   return (
-    <span className={cn('inline-flex items-center gap-1.5', className)}>
+    <span className={cn('inline-flex items-center gap-1', className)}>
       {hasFlag(code) && (
-        <FlagIcon countryCode={code as FlagIconProps['countryCode']} countryName={getCountryName(countryCode, locale)} />
+        <FlagIcon
+          countryCode={code as FlagIconProps['countryCode']}
+          countryName={getCountryName(countryCode, locale)}
+          className=''
+        />
       )}
       {children}
     </span>
