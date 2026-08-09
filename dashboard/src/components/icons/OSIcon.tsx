9b7773a9b052a5179dd47232ecafdfdd792d2070
Christoffer Romild <woldby3@hotmail.com>
2026-08-09 22:14:54 +0200

    Reformatted OSIcon.tsx to satisfy prettier printWidth

--- dashboard/src/components/icons/OSIcon.tsx
+++ dashboard/src/components/icons/OSIcon.tsx
@@ -14,7 +14,14 @@ export function OSIcon({ name, className = 'h-3.5 w-3.5' }: OSIconProps) {
   if (!def) return <Monitor className={cn('shrink-0', className)} />;
 
   if (!def.iconDark) {
-    return <StaticIcon src={`/os-icons/${def.icon.file}`} label={def.label} mono={def.icon.mono} className={className} />;
+    return (
+      <StaticIcon
+        src={`/os-icons/${def.icon.file}`}
+        label={def.label}
+        mono={def.icon.mono}
+        className={className}
+      />
+    );
   }
 
   return (
