4999dbe882cc6fdc35f0a5d9982403c6a6531400
Thomas <thomas.schauser@gmail.com>
2026-09-06 11:00:30 +0200

    Added global properties pre-queue

--- dashboard/src/instrumentation-client.ts
+++ dashboard/src/instrumentation-client.ts
@@ -2,4 +2,7 @@ window.betterlytics = window.betterlytics || {
   event: (...args: unknown[]) => {
     (window.betterlytics!.q = window.betterlytics!.q || []).push(args as unknown as IArguments);
   },
+  setGlobalProperties: (...args: unknown[]) => {
+    (window.betterlytics!.gq = window.betterlytics!.gq || []).push(args as unknown as IArguments);
+  },
 };
--- static/analytics.js
+++ static/analytics.js
@@ -147,6 +147,8 @@
   }
 
   var queuedEvents = (window.betterlytics && window.betterlytics.q) || [];
+  var queuedGlobalProperties =
+    (window.betterlytics && window.betterlytics.gq) || [];
 
   var replayConsentCallbacks = [];
 
@@ -196,6 +198,13 @@
 
   if (initialGlobalProperties !== null) {
     window.betterlytics.setGlobalProperties(initialGlobalProperties);
+  }
+
+  for (var i = 0; i < queuedGlobalProperties.length; i++) {
+    window.betterlytics.setGlobalProperties.apply(
+      this,
+      queuedGlobalProperties[i],
+    );
   }
 
   for (var i = 0; i < queuedEvents.length; i++) {
